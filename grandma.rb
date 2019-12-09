3bf5c4c48cf93a044c674d90b6c7f14988517a4e
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-09 23:12:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,7 @@
 def speak_to_grandma(phrase)
   phrase = "Hello Grandma!"
   
-  if phrase == "HELLO GRANDMA".upcase
-  puts "HUH?! SPEAK UP,SONNY!".upcase
   
-  elsif phrase.upcase == "HELLO GRANDMA!".upcase
-  puts "NO, NOT SINCE 1938!".upcase
-  
-  else "I LOVE YOU GRANDMA!".upcase == "I LOVE YOU GRANDMA!".upcase
-  return "I LOVE YOU TOO PUMPKIN!".upcase
  end
 end 
 
