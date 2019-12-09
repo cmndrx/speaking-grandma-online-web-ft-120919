fee198e60d210eb2e74f85d989320dea75b0c9eb
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-09 23:16:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif prase == phrase.upcase  
+  elsif prase == phrase.upcase
+    return "NO"
   
   
  end
